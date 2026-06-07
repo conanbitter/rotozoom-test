f345cd5031bc26147a6e357610415d505548c8f5
Conan Bitter <conan.bitter@gmail.com>
2026-06-07 11:16:11 +0300

    Add buffer

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,17 @@ use minifb::{Scale, Window, WindowOptions};
 const WINDOW_WIDTH: usize = 800;
 const WINDOW_HEIGHT: usize = 600;
 
+fn update_buffer(buffer: &mut [u32]) {
+    for y in 0..WINDOW_HEIGHT {
+        for x in 0..WINDOW_WIDTH {
+            let r = (255.0 * x as f32 / WINDOW_WIDTH as f32) as u32;
+            let g = (255.0 * y as f32 / WINDOW_HEIGHT as f32) as u32;
+            let color = r << 16 | g << 8 | 128;
+            buffer[x + y * WINDOW_WIDTH] = color;
+        }
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let mut window = Window::new(
         "RotoZoom",
@@ -25,8 +36,11 @@ fn main() -> anyhow::Result<()> {
 
     window.set_target_fps(60);
 
+    let mut buffer: Vec<u32> = vec![0; WINDOW_WIDTH * WINDOW_HEIGHT];
+    update_buffer(&mut buffer);
+
     while window.is_open() && !window.is_key_down(minifb::Key::Escape) {
-        window.update();
+        window.update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT)?;
     }
 
     Ok(())
